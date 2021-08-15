f9d4576b4e5d90add3ed4162f3367c1be40f9834
Pulasthi Ishara <pulasthi.ishara@zeropoint.hr>
2021-08-16 00:25:42 +0530

    cors updated

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: 'http://localhost:4200'
+    }
+  });
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
